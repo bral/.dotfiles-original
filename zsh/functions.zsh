a5ae083076a5895c189a90acdd35e217649b267f
--git <michaelvanasse@gmail.com>
2013-12-14 02:31:35 -0700

    add tmux window & pane support for children and animals

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,7 +7,11 @@ function cd () {
   builtin cd "$@";
   clear;
   lsr;
-  set-tab-title "`basename $PWD`"
+  if [ ! -z $TMUX ]; then
+    set-window-title "`basename $PWD`"
+  else
+    set-tab-title "`basename $PWD`"
+  fi
 }
 
 function set-tab-title {
@@ -16,4 +20,18 @@ function set-tab-title {
   zformat -f title_formatted "$title_format" "s:$argv"
 
   printf "\e]1;%s\a" ${(V%)title_formatted}
+}
+
+function set-window-title {
+  local title_format{,ted}
+  zstyle -s ':prezto:module:terminal:window-title' format 'title_format' || title_format="%s"
+  zformat -f title_formatted "$title_format" "s:$argv"
+
+  if [[ "$TERM" == screen* ]]; then
+    title_format="\ek%s\e\\"
+  else
+    title_format="\e]2;%s\a"
+  fi
+
+  printf "$title_format" "${(V%)title_formatted}"
 }
