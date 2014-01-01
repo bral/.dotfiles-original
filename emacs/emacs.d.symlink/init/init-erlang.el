ac85814ae5688ddd9c34cba55bae53ee7ac6832e
CamShaft <bytheway.cameron@gmail.com>
2013-12-31 23:05:47 -0700

    set indentation level for erlang

--- emacs/emacs.d.symlink/init/init-erlang.el
+++ emacs/emacs.d.symlink/init/init-erlang.el
@@ -6,4 +6,6 @@
   (add-to-list 'auto-mode-alist '("\\.yrl\\'" . erlang-mode))
   (add-to-list 'auto-mode-alist '("\\.xrl\\'" . erlang-mode)))
 
+(setq erlang-indent-level 2)
+
 (provide 'init-erlang)
