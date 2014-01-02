496c39591c216af3d2b066a0bd074e20f0c6712c
CamShaft <bytheway.cameron@gmail.com>
2014-01-02 02:16:25 -0700

    fix the whitespace crap

--- emacs/emacs.d.symlink/init/init-whitespace.el
+++ emacs/emacs.d.symlink/init/init-whitespace.el
@@ -2,9 +2,6 @@
 (add-hook 'makefile-mode-hook 'indent-tabs-mode)
 
 (setq-default show-trailing-whitespace t)
-
-;; cleanup any trailing any whitespace
-(require-package 'whitespace-cleanup-mode)
-(global-whitespace-cleanup-mode t)
+(setq-default indent-tabs-mode nil)
 
 (provide 'init-whitespace)
