eb5e10d13e6833e8a036187c29673ac4af642822
--git <mail@mndvns.com>
2014-03-19 23:16:33 -0600

    js updates

--- emacs/emacs.d.symlink/init/init-shell.el
+++ emacs/emacs.d.symlink/init/init-shell.el
@@ -1,3 +1,8 @@
 (global-set-key (kbd "A-;") 'shell-command)
 
+;; (add-hook 'shell-script-mode-hook
+;;       '(lambda()
+;;         (setq c-basic-offset 2)
+;;         (setq indent-tabs-mode nil))
+
 (provide 'init-shell)
