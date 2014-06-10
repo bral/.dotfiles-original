db4a8444726c90c07ff49cd788f4fc40def32c7c
camshaft <bytheway.cameron@gmail.com>
2014-06-10 17:32:29 -0600

    set the cursor type to 'bar

--- emacs/emacs.d.symlink/init/init-appearance.el
+++ emacs/emacs.d.symlink/init/init-appearance.el
@@ -9,6 +9,8 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
+(setq-default cursor-type 'bar)
+
 ;; Setup GUI mode
 (when window-system
   (setq frame-title-format '(buffer-file-name "%f" ("%b")))
