766ab04d3ccdca297ef43ea1a1ea1ae2fee77e64
--git <mail@mndvns.com>
2014-06-25 10:52:20 -0600

    turn off trailing whitespace

--- emacs/emacs.d.symlink/init/init-whitespace.el
+++ emacs/emacs.d.symlink/init/init-whitespace.el
@@ -1,7 +1,7 @@
 ;; Use normal tabs in makefiles
 (add-hook 'makefile-mode-hook 'indent-tabs-mode)
 
-(setq-default show-trailing-whitespace t)
+(setq-default show-trailing-whitespace nil)
 (setq-default indent-tabs-mode nil)
 
 (provide 'init-whitespace)
