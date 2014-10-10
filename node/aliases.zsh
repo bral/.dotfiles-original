dc77cad0b229e0c0365ca0f7013f0c8c6bbb8ae1
--git <mail@mndvns.com>
2014-10-10 14:58:52 -0600

    fix `nr`

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -5,10 +5,6 @@ if [[ `node -v` =~ ^v0.11 ]]; then
   alias _node="/usr/local/bin/node --harmony"
 fi
 
-if (( $+commands[node] )); then
-  npm install -g nr;
-fi
-
 function node(){
   if [[ $# == 0 ]]; then
     nr;
