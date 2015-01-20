930357868aab87ce7df5c786b224679fa382b51c
mndvns <mail@mndvns.com>
2015-01-20 14:30:57 -0700

    fix burl install

--- burl/install.sh
+++ burl/install.sh
@@ -1,5 +1,6 @@
+#!/usr/local/bin/zsh
 # Install burl
 
-if test ! (( $+command[burl] )); then
+if [[ ! -x `which burl` ]]; then
   mkdir /tmp/burl && cd /tmp/burl && curl -L# https://github.com/visionmedia/burl/archive/master.tar.gz | tar zx --strip 1 && make install
 fi
