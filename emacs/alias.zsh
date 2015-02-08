1ff630977326393d2c0735e40791f58997b11879
mndvns <mail@mndvns.com>
2015-02-08 08:57:10 -0700

    remove unused emacs contribs

--- emacs/alias.zsh
+++
@@ -1,4 +0,0 @@
-function emacs-debug() {
-  test ! -x /Applications/Emacs.app/Contents/MacOS/Emacs && >&2 echo "cannot find emacs" && return 1
-  /Applications/Emacs.app/Contents/MacOS/Emacs --debug-init $@ &
-}
--- emacs/spacemacs/contribs/adaptive-wrap/extensions.el
+++
@@ -1,7 +0,0 @@
-(defvar adaptive-wrap-pre-extensions
-  '(
-    adaptive-wrap
-    ))
-
-(defun adaptive-wrap/init-adaptive-wrap ()
-  (require 'adaptive-wrap))
